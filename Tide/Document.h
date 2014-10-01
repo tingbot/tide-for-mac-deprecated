b28804b069a87f7700c8aa877cf5bcbb0d37dbd0
Joe Rickerby <joerick@mac.com>
2014-10-01 12:03:45 +0100

    Initial Commit

---
+++ Tide/Document.h
@@ -0,0 +1,15 @@
+//
+//  Document.h
+//  Tide
+//
+//  Created by Joe Rickerby on 01/10/2014.
+//  Copyright (c) 2014 Tingbot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+
+@end
+
---
+++ Tide/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Tide
+//
+//  Created by Joe Rickerby on 01/10/2014.
+//  Copyright (c) 2014 Tingbot. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
