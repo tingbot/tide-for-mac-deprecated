413c6256789eabca001c7d05c20d518e0518e1c0
Joe Rickerby <joerick@mac.com>
2015-04-23 18:49:00 +0100

    Update default

--- Tide/default.tingapp/main.py
+++ Tide/default.tingapp/main.py
@@ -1,11 +1,13 @@
 import tingbot
 from tingbot import screen
 
-def setup():
-    pass
+# setup code here
+screen.fill(color='black')
 
 def loop():
+    # drawing code here
     pass
 
+# run the app
 import sys
 tingbot.run(sys.modules[__name__])
