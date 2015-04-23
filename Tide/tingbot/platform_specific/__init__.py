4390cadc7d25617b4a8a266c8d6bf983066cec51
Joe Rickerby <joerick@mac.com>
2015-04-23 17:59:44 +0100

    hide cursor

--- Tide/tingbot/platform_specific/__init__.py
+++ Tide/tingbot/platform_specific/__init__.py
@@ -5,5 +5,5 @@ if platform.system() == 'Darwin':
     fixup_window = osx.fixup_window
     fixup_env = osx.fixup_env
 else:
-    fixup_window = lambda: None
+    fixup_window = pi.fixup_window
     fixup_env = pi.fixup_env
--- Tide/tingbot/platform_specific/pi.py
+++ Tide/tingbot/platform_specific/pi.py
@@ -3,3 +3,7 @@ import os
 
 def fixup_env():
     os.environ["SDL_FBDEV"] = "/dev/fb1"
+
+def fixup_window():
+    import pygame.mouse
+    pygame.mouse.set_visible(0)
