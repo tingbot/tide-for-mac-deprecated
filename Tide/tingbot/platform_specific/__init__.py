abb9c7eb2b3cd4a85f63ae98aa86681333e8fd9c
Joe Rickerby <joerick@mac.com>
2015-10-30 20:52:38 +0000

    Tingbot module is now in a separate repo, submodule to Tide

--- Tide/tingbot/platform_specific/__init__.py
+++
@@ -1,6 +0,0 @@
-import platform
-
-if platform.system() == 'Darwin':
-    from osx import fixup_env, fixup_window, register_button_callback
-else:
-    from pi import fixup_env, fixup_window, register_button_callback
